56a2dd3658a478ef2284dbd0e9afba073881bb38
Muslim Turanov <muslimturanov25@outlook.com>
2025-11-11 12:00:31 +0500

    detector v1.0 w/o timer

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import cv2
-from ultralytics import YOLO 
-import torch
-model = YOLO("yolov5s.pt") 
-cap = cv2.VideoCapture("test.mp4")
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-    results = model(frame)  
-    annotated_frame = results[0].plot()  
-    cv2.imshow("YOLO Detection", annotated_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+from yolo.detector import run_detector
+
+if __name__ == "__main__":
+    run_detector()
